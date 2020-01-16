c4266b25402013a464e55ce7674c5cc130fade44
kzcheng <kevinzifancheng@gmail.com>
2020-01-16 14:43:34 -0800

    Rewritten some comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,8 +3,10 @@ const request = require(`request`);
 
 const fetchBreedDescription = function(breed, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // Hint the programmer about errors
+    console.log('error:', error);
+    // Hint the programmer about response status codes
+    console.log('statusCode:', response && response.statusCode);
 
     // dealing with the main body
     callback(error, JSON.parse(body));
